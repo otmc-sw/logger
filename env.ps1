fe5769c3ecde1435b52f5c9922eb476e5ccc79df
Trung Nguyen Van <nguyentrungmx27@gmail.com>
2026-07-04 17:11:58 +0700

    Init: Add test script

--- env.ps1
+++ env.ps1
@@ -10,6 +10,7 @@ $global:TOP = Get-Location
 function e   { Set-Location $TOP; & "$TOP\env.ps1" @args }
 function p   { Set-Location $TOP; & "$TOP\scripts\push.ps1" @args }
 function tag { Set-Location $TOP; & "$TOP\scripts\tag.ps1" @args }
+function t   { Set-Location $TOP; & "$TOP\scripts\test.ps1" @args }
 
 
 function Show-Help {
@@ -21,12 +22,14 @@ function Show-Help {
     Write-Host "  COMMANDS:" -ForegroundColor Yellow
     Write-Host "    e                     Load environment"
     Write-Host "    p                     Push changes to remote repository"
+    Write-Host "    t                     Run tests"
     Write-Host "    tag b <tag>           Create tag at current HEAD"
     Write-Host "    tag r <tag>           Restore current branch to tag (force push)"
     Write-Host ""
     Write-Host "  EXAMPLES:" -ForegroundColor Yellow
     Write-Host "    tag b v0.1.1          Create tag v0.1.1 at HEAD"
     Write-Host "    tag r v0.1.1          Restore branch to tag v0.1.1"
+    Write-Host "    t                     Run tests"
     Write-Host ""
 }
 
---
+++ scripts/test.ps1
@@ -0,0 +1,8 @@
+#
+# Apache License 2.0
+# Copyright (c) 2026 OTMC Softwares.
+# Contributors: Nguyen Van Trung, Nguyen Thi Hoai, OTMC Contributors.
+#
+
+Set-Location $PSScriptRoot/..
+go run tests/print_logs.go
