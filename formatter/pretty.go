5df557ea60b012ae5c19338beab8d3cb87596932
Trung Nguyen Van <nguyentrungmx27@gmail.com>
2026-07-05 10:19:47 +0700

    Improve: Update files.

--- formatter/pretty.go
+++ formatter/pretty.go
@@ -59,7 +59,7 @@ func (f *PrettyFormatter) FormatRequest(req internal.Request) string {
 	}
 
 	return fmt.Sprintf(
-		"%s |%s| %s | %s | %-12s | %s\n",
+		"%s |%s| %s | %s | %-15s | %s\n",
 		timestamp,
 		method,
 		latency,
