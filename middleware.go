f87572fc181dc2564b47a6773d007e52b51ac30b
Trung Nguyen Van <nguyentrungmx27@gmail.com>
2026-07-06 11:44:30 +0700

    Chore: Remove middleware

--- middleware.go
+++
@@ -1,6 +0,0 @@
-﻿/**
- * @License Apache License 2.0
- * @Copyright (c) 2026 OTMC Softwares. OTMC Golang Logger.
- * @Contributors Nguyen Van Trung, Nguyen Thi Hoai, OTMC Contributors.
-**/
-package logger
