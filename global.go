19fd1fa70413bfb1d809022bf4dad37694976764
Trung Nguyen Van <nguyentrungmx27@gmail.com>
2026-07-07 10:26:57 +0700

    Improve: Update files.

--- global.go
+++ global.go
@@ -14,67 +14,6 @@ import (
 type Formatter = core.Formatter
 type Hook = core.Hook
 
-type Level = core.Level
-
-const (
-	TraceLevel = core.TraceLevel
-	DebugLevel = core.DebugLevel
-	InfoLevel  = core.InfoLevel
-	WarnLevel  = core.WarnLevel
-	ErrorLevel = core.ErrorLevel
-	CritLevel  = core.CritLevel
-)
-
-func ParseLevel(level string) Level {
-	switch level {
-	case "TRACE":
-		return TraceLevel
-	case "DEBUG":
-		return DebugLevel
-	case "INFO":
-		return InfoLevel
-	case "WARN":
-		return WarnLevel
-	case "ERROR":
-		return ErrorLevel
-	case "CRIT":
-		return CritLevel
-	default:
-		return InfoLevel
-	}
-}
-
-type Config struct {
-	Level      Level
-	Console    bool
-	File       bool
-	Filename   string
-	JSON       bool
-	Caller     bool
-	Stacktrace bool
-	MaxSize    int
-	MaxBackups int
-	MaxAge     int
-	Compress   bool
-	TimeFormat string
-}
-
-func DefaultConfig() Config {
-	return Config{
-		Level:      InfoLevel,
-		Console:    true,
-		File:       false,
-		JSON:       false,
-		Caller:     true,
-		Stacktrace: false,
-		MaxSize:    10,
-		MaxBackups: 3,
-		MaxAge:     90,
-		Compress:   true,
-		TimeFormat: "2006-01-02 15:04:05.000 -07:00",
-	}
-}
-
 func Trace(format string, args ...any) {
 	global.core.Log(core.TraceLevel, 3, format, args...)
 }
---
+++ level.go
@@ -0,0 +1,39 @@
+﻿/**
+ * @License Apache License 2.0
+ * @Copyright (c) 2026 OTMC Softwares. OTMC Golang Logger.
+ * @Contributors Nguyen Van Trung, Nguyen Thi Hoai, OTMC Contributors.
+**/
+
+package logger
+
+import "github.com/otmc-sw/logger/core"
+
+type Level = core.Level
+
+const (
+	TraceLevel = core.TraceLevel
+	DebugLevel = core.DebugLevel
+	InfoLevel  = core.InfoLevel
+	WarnLevel  = core.WarnLevel
+	ErrorLevel = core.ErrorLevel
+	CritLevel  = core.CritLevel
+)
+
+func ParseLevel(level string) Level {
+	switch level {
+	case "TRACE":
+		return TraceLevel
+	case "DEBUG":
+		return DebugLevel
+	case "INFO":
+		return InfoLevel
+	case "WARN":
+		return WarnLevel
+	case "ERROR":
+		return ErrorLevel
+	case "CRIT":
+		return CritLevel
+	default:
+		return InfoLevel
+	}
+}
